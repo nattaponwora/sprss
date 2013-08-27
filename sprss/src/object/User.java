c1c3bd0d136f9ade09cfaef6619296843b1cf8b8
nattaponwora <nattapon_wora@hotmail.com>
2013-08-27 10:17:29 +0700

    สร้างตัวแปรฟิลด์ใน User.java

--- sprss/src/object/User.java
+++ sprss/src/object/User.java
@@ -1,5 +1,14 @@
 package object;
 
 public class User {
-
+	private int id;
+	private String firstname;
+	private String lastname;
+	private int eid;
+	private String email;
+	private String posotion;
+	
+	public User(){
+		
+	}
 }
