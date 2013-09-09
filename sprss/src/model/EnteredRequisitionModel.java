0fb4cf97e6cc0a175864525b230a9f1d3eed6c36
POM <POM@POM-PC>
2013-09-09 13:30:59 +0700

    update Model

--- sprss/src/model/EnteredRequisitionModel.java
+++ sprss/src/model/EnteredRequisitionModel.java
@@ -23,23 +23,26 @@ public class EnteredRequisitionModel {
 		String lastname = "วรศักดาพิศาล";
 		String empID = "97912531";
 		String email = "nattapon_wora@hotmail.com";
+		String tel = "0856667777";
 		String usergroup = "admin";
-		picker.add( new User( id, firstname, lastname, empID, email, usergroup, plant, storeroom ) ) ;
+		picker.add( new User( id, firstname, lastname, empID, tel, email, usergroup, plant, storeroom ) ) ;
 		
 		id = 2;
 		firstname = "มานะ" ;
 		lastname = "ไปใหน";
 		empID = "97912532";
 		email = "mana_painhi@hotmail.com";
+		tel = "0823334444";
 		usergroup = "picker";
-		picker.add( new User( id, firstname, lastname, empID, email, usergroup, plant, storeroom ) ) ;
+		picker.add( new User( id, firstname, lastname, empID, tel, email, usergroup, plant, storeroom ) ) ;
 		
 		id = 3;
 		firstname = "โสฬส" ;
 		lastname = "ไปเทพพงค์ชัย";
 		empID = "97912533";
 		email = "little_angle@hotmail.com";
-		picker.add( new User( id, firstname, lastname, empID, email, usergroup, plant, storeroom ) ) ;
+		tel = "0889999999";
+		picker.add( new User( id, firstname, lastname, empID, tel, email, usergroup, plant, storeroom ) ) ;
 		
 		return picker;
 	}
@@ -56,7 +59,7 @@ public class EnteredRequisitionModel {
 		Item i = new Item(70000, 1, "Item1" , "S12-01-01-02" , 1 , 97912531);
 		Item j = new Item(70000, 2, "Item2" , "S12-01-01-03" , 2 , 97912531);
 		Item k = new Item(70000, 3, "Item3" , "S12-03-04-03" , 1 , 97912531);
-		Requisition r = new Requisition( 97912531, "0301", 70000 );
+		Requisition r = new Requisition( 97912531, "สามี", "นาศรี", "E1", 70000, "02/09/2013", null ,"0301");
 		r.addItem(i);
 		r.addItem(j);
 		r.addItem(k);
@@ -64,14 +67,14 @@ public class EnteredRequisitionModel {
 		i = new Item(70001, 2, "Item2" , "S12-01-01-03" , 1 , 97912532);
 		j = new Item(70001, 3, "Item3" , "S12-03-04-03" , 1 , 97912532);
 		k = new Item(70001, 4, "Item4" , "S12-04-01-02" , 2 , 97912532);
-		r = new Requisition( 97912532, "0301", 70001 );
+		r = new Requisition( 97912532, "เนวิน", "ชิดชอบ", "E1", 70001, "02/09/2013", null ,"0301" );
 		r.addItem(i);
 		r.addItem(j);
 		r.addItem(k);
 		reqList.add(r);
 		i = new Item(70002, 1, "Item1" , "S12-01-01-02" , 4 , 97912534);
 		j = new Item(70002, 2, "Item2" , "S12-01-01-03" , 3 , 97912534);
-		r = new Requisition( 97912534, "0301", 70002 );
+		r = new Requisition( 97912534, "ดนัย", "อุดมโชค", "E1", 70002, "02/09/2013", null ,"0301" );
 		r.addItem(i);
 		r.addItem(j);
 		reqList.add(r);
