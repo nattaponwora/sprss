98905eec7dfced267432346f7070bf5698be2bc7
nattaponwora <nattapon_wora@hotmail.com>
2013-09-26 17:39:36 +0700

    mxmsync

--- sprss/src/model/MXMSyncModel.java
+++ sprss/src/model/MXMSyncModel.java
@@ -12,26 +12,47 @@ public class MXMSyncModel {
 	public static String getDataFromMXM() throws SQLException{
 		Statement db2Stm = StatementManager.getDB2Statement();
 		Statement sqlStm = StatementManager.getSQLStatement();
-		String sql = "SELECT iu.INVUSENUM req_id, iu.GP_PDA_USERID resv_id, p.DISPLAYNAME resv_name, iu.GP_PDA_TEAM resv_team, iu.SITEID plant ,iu.FROMSTORELOC storeroom, iu.GP_PDA_UPDATEDDATE enterdate, iu.USETYPE type "
-				+ "FROM invuse iu join PERSON p ON iu.GP_PDA_USERID = p.PERSONID "
-				+ "WHERE iu.GP_PDA_USERID is not null AND iu.status = 'ENTERED' ";
+		String siteQuery = "SELECT plant, storeroom FROM warehouse";
+		ResultSet storeRS = sqlStm.executeQuery(siteQuery);
+		String sql =  "SELECT iu.INVUSENUM req_id,il.ITEMNUM, il.DESCRIPTION des, inv.GP_BINNUM, il.QUANTITY amount, iu.GP_PDA_USERID resv_id, p.DISPLAYNAME resv_name, iu.GP_PDA_TEAM resv_team, iu.SITEID plant ,iu.FROMSTORELOC storeroom, iu.GP_PDA_UPDATEDDATE enterdate, iu.USETYPE type "
+					+ "FROM invuse iu "
+					+ "join INVUSELINE il on iu.INVUSENUM = il.INVUSENUM "
+					+ "join INVENTORY inv on inv.ITEMNUM = il.ITEMNUM AND inv.SITEID = iu.SITEID AND inv.LOCATION = iu.FROMSTORELOC "
+					+ "join PERSON p ON iu.GP_PDA_USERID = p.PERSONID "
+					+ "WHERE iu.GP_PDA_USERID is not null AND iu.status = 'ENTERED' AND iu.GP_PDA_UPDATEDDATE > date('2013-09-01')  AND "
+					+ "( ";
+		boolean first = true;
+		while( storeRS.next() ){
+			if( ! first ){
+				sql += "OR";
+			}else{
+				first = false;
+			}
+   			sql += " ( iu.SITEID = '"+ storeRS.getString("plant")  +"' and iu.FROMSTORELOC = '"+ storeRS.getString("storeroom") +"' ) ";
+		}
+		sql +=  ") "
+				+ "ORDER BY iu.INVUSENUM";
+		
 		ResultSet rs = db2Stm.executeQuery(sql);
 		RequisitionList rl = new RequisitionList();
-		rs.last(); 
-		int total = rs.getRow();
-		rs.beforeFirst();
-		//int reqIDList[] = new int[total];
+
 		while( rs.next() ){
-			int req_id = rs.getInt("req_ID");
-			int resv_id = rs.getInt("resv_id");
-			String resv_name = rs.getString("resv_name");
-			String resv_team = rs.getString("resv_team");
-			String plant = rs.getString("plant");
-			String storeroom = rs.getString("storeroom");
-			Date enterdate = rs.getDate("enterdate");
-			String type = rs.getString("type");
-			Requisition r = new Requisition(req_id, resv_id, resv_name, resv_team, enterdate, plant,storeroom , "entered", type);
-			rl.add(r);
+			int req_id = rs.getInt("resv_id");
+			Requisition r;
+			if( rl.getByID(req_id) == null ){
+				int resv_id = rs.getInt("resv_id");
+				String resv_name = rs.getString("resv_name");
+				String resv_team = rs.getString("resv_team");
+				String plant = rs.getString("plant");
+				String storeroom = rs.getString("storeroom");
+				Date enterdate = rs.getDate("enterdate");
+				String type = rs.getString("type");
+				r = new Requisition(req_id, resv_id, resv_name, resv_team, enterdate, plant,storeroom , "entered", type);
+				rl.add(r);
+			}else{
+				r = rl.getByID(req_id);
+			}
+			
 		}
 		
 		sql =     "SELECT "
@@ -42,8 +63,9 @@ public class MXMSyncModel {
 		
 		
 		String s = rs.getString("DISPLAYNAME");
-		stm.close();
-		return s;
+		//stm.close();
+		*/
+		return sql;
 		//stm = StatementManager.getSQLStatement();
 	}
 	
